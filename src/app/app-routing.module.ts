818898800576257c6d1550fc045fa12bb75d511d
Chiara Zuffi <c.zuffi@cineca.it>
2023-03-10 11:52:15 +0100

    new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ItemDetailComponent } from './items/item-detail/item-detail.component';
+import { ItemStartComponent } from './items/item-start/item-start.component';
+import { ItemsComponent } from './items/items.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/items', pathMatch: 'full' },
+  { path: 'items', component: ItemsComponent, children: [
+    { path: '', component: ItemStartComponent },
+    { path: ':id', component: ItemDetailComponent }
+  ] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemsComponent } from './items/items.component';
+import { ItemDetailComponent } from './items/item-detail/item-detail.component';
+import { HeaderComponent } from './header/header.component';
+import { ItemListComponent } from './items/item-list/item-list.component';
+import { ItemStartComponent } from './items/item-start/item-start.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent,
+    ItemDetailComponent,
+    HeaderComponent,
+    ItemListComponent,
+    ItemStartComponent
   ],
   imports: [
     BrowserModule,
