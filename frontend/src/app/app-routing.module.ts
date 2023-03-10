607b72b56a3658168bfbaeae6aa717bb5b9ec951
Chiara Zuffi <c.zuffi@cineca.it>
2023-03-10 14:39:04 +0100

    item edit and routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ItemDetailComponent } from './items/item-detail/item-detail.component';
+import { ItemEditComponent } from './items/item-edit/item-edit.component';
 import { ItemStartComponent } from './items/item-start/item-start.component';
 import { ItemsComponent } from './items/items.component';
 
@@ -8,7 +9,9 @@ const routes: Routes = [
   { path: '', redirectTo: '/items', pathMatch: 'full' },
   { path: 'items', component: ItemsComponent, children: [
     { path: '', component: ItemStartComponent },
-    { path: ':id', component: ItemDetailComponent }
+    { path: 'new', component: ItemEditComponent },
+    { path: ':id', component: ItemDetailComponent },
+    { path: ':id/edit', component: ItemEditComponent },
   ] }
 ];
 
