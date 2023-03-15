1b5472ee8c9083caccebe3415440b909acfed787
Chiara Zuffi <c.zuffi@cineca.it>
2023-03-15 22:24:24 +0100

    edit item component

--- frontend/src/app/items/item-edit/item-edit.component.ts
+++ frontend/src/app/items/item-edit/item-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Item } from '../item.model';
+import { ItemService } from '../item.service';
 
 @Component({
   selector: 'app-item-edit',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-edit.component.css']
 })
 export class ItemEditComponent implements OnInit {
+  id: number;
+  editMode = false;
 
-  constructor() { }
+  item: Item;
+  itemPicture = '';
+  itemName = '';
+  itemDescription = '';
+  itemPrice = 1;
+  itemNotes = [];
+
+  constructor(private route: ActivatedRoute, private itemService: ItemService ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        this.editMode = (params['id']) ? true : false;
+      }
+    )
+
+    if (this.editMode) {
+      this.item = this.itemService.getItem(this.id);
+      this.itemPicture = this.item.picture;
+      this.itemName = this.item.name;
+      this.itemDescription = this.item.description;
+      this.itemPrice = this.item.price;
+      this.itemNotes = this.item.notes;
+    }
+  }
+
+  onSaveItem() {
+
   }
 
 }
